f46c1553d94b9f61754103bee7214b353a03fead
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-08 08:55:46 -0300

    :tada: initial structure

---
+++ lib/freelas_parallelism.ex
@@ -0,0 +1,18 @@
+defmodule FreelasParallelism do
+  @moduledoc """
+  Documentation for `FreelasParallelism`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FreelasParallelism.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/freelas_parallelism_test.exs
@@ -0,0 +1,8 @@
+defmodule FreelasParallelismTest do
+  use ExUnit.Case
+  doctest FreelasParallelism
+
+  test "greets the world" do
+    assert FreelasParallelism.hello() == :world
+  end
+end
